f21c41143fd44c4159d411ef7fb1eb215cbd7714
Valkryst <valkryst@valkryst.com>
2016-07-20 13:14:41 -0300

    Replaced original unit tests.
    Two of the new tests ensure backwards compatibility with the old generateName method which takes in Random instead of IntUnaryOperator.
    
    The remaining test runs 1,000,000 name generations.

--- test/com/valkryst/generator/ConsonantVowelNameGeneratorTest.java
+++ test/com/valkryst/generator/ConsonantVowelNameGeneratorTest.java
@@ -6,65 +6,110 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.function.IntUnaryOperator;
 
 public class ConsonantVowelNameGeneratorTest {
+    private static final List<String> CONSONANTS = new ArrayList<>();
+
+    private static final List<String> VOWELS = new ArrayList<>();
+
+    static {
+        CONSONANTS.add("al");
+        CONSONANTS.add("an");
+        CONSONANTS.add("ar");
+        CONSONANTS.add("as");
+        CONSONANTS.add("at");
+        CONSONANTS.add("ea");
+        CONSONANTS.add("ed");
+        CONSONANTS.add("en");
+        CONSONANTS.add("er");
+        CONSONANTS.add("es");
+        CONSONANTS.add("ha");
+        CONSONANTS.add("he");
+        CONSONANTS.add("hi");
+        CONSONANTS.add("in");
+        CONSONANTS.add("is");
+        CONSONANTS.add("it");
+        CONSONANTS.add("le");
+        CONSONANTS.add("me");
+        CONSONANTS.add("nd");
+        CONSONANTS.add("ne");
+        CONSONANTS.add("ng");
+        CONSONANTS.add("nt");
+        CONSONANTS.add("on");
+        CONSONANTS.add("or");
+        CONSONANTS.add("ou");
+        CONSONANTS.add("re");
+        CONSONANTS.add("se");
+        CONSONANTS.add("st");
+        CONSONANTS.add("te");
+        CONSONANTS.add("th");
+        CONSONANTS.add("ti");
+        CONSONANTS.add("to");
+        CONSONANTS.add("ve");
+        CONSONANTS.add("wa");
+        CONSONANTS.add("it");
+
+        VOWELS.add("a");
+        VOWELS.add("e");
+        VOWELS.add("i");
+        VOWELS.add("o");
+        VOWELS.add("u");
+        VOWELS.add("y");
+    }
+
     @Test
-    public void generate() {
+    public void generateNameUsingRandom() {
         final Random random = new Random(System.currentTimeMillis());
 
-        final List<String> consonants = new ArrayList<>();
-        consonants.add("al");
-        consonants.add("an");
-        consonants.add("ar");
-        consonants.add("as");
-        consonants.add("at");
-        consonants.add("ea");
-        consonants.add("ed");
-        consonants.add("en");
-        consonants.add("er");
-        consonants.add("es");
-        consonants.add("ha");
-        consonants.add("he");
-        consonants.add("hi");
-        consonants.add("in");
-        consonants.add("is");
-        consonants.add("it");
-        consonants.add("le");
-        consonants.add("me");
-        consonants.add("nd");
-        consonants.add("ne");
-        consonants.add("ng");
-        consonants.add("nt");
-        consonants.add("on");
-        consonants.add("or");
-        consonants.add("ou");
-        consonants.add("re");
-        consonants.add("se");
-        consonants.add("st");
-        consonants.add("te");
-        consonants.add("th");
-        consonants.add("ti");
-        consonants.add("to");
-        consonants.add("ve");
-        consonants.add("wa");
-        consonants.add("it");
-
-        final List<String> vowels = new ArrayList<>();
-        vowels.add("a");
-        vowels.add("e");
-        vowels.add("i");
-        vowels.add("o");
-        vowels.add("u");
-        vowels.add("y");
-
+        // Setup the Builder:
         final ConsonantVowelNameGeneratorBuilder builder = new ConsonantVowelNameGeneratorBuilder();
-        builder.setConsonants(consonants);
-        builder.setVowels(vowels);
+        builder.setConsonants(CONSONANTS);
+        builder.setVowels(VOWELS);
 
+        // Setup & Test the Generator:
         final ConsonantVowelNameGenerator generator = builder.build();
 
-        for(int i = 0 ; i < 10 ; i++) {
-            System.out.println(generator.generateName(random, 6));
+        for(int i = 0 ; i < 100 ; i++) {
+            generator.generateName(random, i % 20);
+        }
+    }
+
+    @Test
+    public void generateNameUsingIntUnaryOperator() {
+        final IntUnaryOperator randomInRange = (value) -> new Random(System.nanoTime()).nextInt(value);
+
+        // Setup the Builder:
+        final ConsonantVowelNameGeneratorBuilder builder = new ConsonantVowelNameGeneratorBuilder();
+        builder.setConsonants(CONSONANTS);
+        builder.setVowels(VOWELS);
+
+        // Setup & Test the Generator:
+        final ConsonantVowelNameGenerator generator = builder.build();
+
+        for(int i = 0 ; i < 100 ; i++) {
+            generator.generateName(randomInRange, i % 20);
+        }
+    }
+
+    @Test
+    public void longGeneration() {
+        final IntUnaryOperator randomInRange = (value) -> new Random(System.nanoTime()).nextInt(value);
+
+        // Setup the Builder:
+        final ConsonantVowelNameGeneratorBuilder builder = new ConsonantVowelNameGeneratorBuilder();
+        builder.setConsonants(CONSONANTS);
+        builder.setVowels(VOWELS);
+
+        // Setup & Test the Generator:
+        final ConsonantVowelNameGenerator generator = builder.build();
+
+        for (int i = 0 ; i < 100 ; i++) {
+            System.out.println(generator.generateName(randomInRange, i));
+        }
+
+        for (int i = 0 ; i < 1_000_000 ; i++) {
+            generator.generateName(randomInRange, i % 20);
         }
     }
 }
