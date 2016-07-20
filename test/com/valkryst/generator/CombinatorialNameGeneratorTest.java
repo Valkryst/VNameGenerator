c42f476d6f5141c8ad8f029817e34aec68f31d4b
Valkryst <valkryst@valkryst.com>
2016-07-20 13:08:10 -0300

    Replaced original unit tests.
    Two of the new tests ensure backwards compatibility with the old generateName method which takes in Random instead of IntUnaryOperator.
    
    The remaining test runs 1,000,000 name generations.

--- test/com/valkryst/generator/CombinatorialNameGeneratorTest.java
+++ test/com/valkryst/generator/CombinatorialNameGeneratorTest.java
@@ -4,68 +4,78 @@ import com.valkryst.TestHelper;
 import com.valkryst.builder.CombinatorialNameGeneratorBuilder;
 import org.junit.Test;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Random;
+import java.util.function.IntUnaryOperator;
 
 public class CombinatorialNameGeneratorTest {
     @Test
-    public void generateNameFantasyDwarven() {
+    public void generateNameUsingRandom() {
+        final Random random = new Random(System.currentTimeMillis());
+
+        // Setup the Builder:
+        final CombinatorialNameGeneratorBuilder builder = new CombinatorialNameGeneratorBuilder();
+
         try {
-            final Random random = new Random(System.currentTimeMillis());
-
-            final CombinatorialNameGeneratorBuilder builder = new CombinatorialNameGeneratorBuilder();
-
             builder.setBeginnings(TestHelper.resource("Dwarven/Fantasy/Dwarven_Khordaldrum_Fantasy_Last_A.txt"));
             builder.setEndings(TestHelper.resource("Dwarven/Fantasy/Dwarven_Khordaldrum_Fantasy_Last_B.txt"));
-
-            final CombinatorialNameGenerator nameGenerator = builder.build(false);
-
-            for(int i = 0 ; i < 50 ; i++) {
-                System.out.println(nameGenerator.generateName(random,  i, 0));
-            }
-
-            for(int i = 0 ; i < 10000 ; i++) {
-                nameGenerator.generateName(random, i, 0);
-            }
-        } catch(final IOException e) {
+        } catch (final IOException e) {
             e.printStackTrace();
+        }
+
+        // Setup & Test the Generator:
+        final CombinatorialNameGenerator generator = builder.build(false);
+
+        for (int i = 0 ; i < 100 ; i++) {
+            generator.generateName(random, i, 0);
         }
     }
 
     @Test
-    public void generateNameRealisticDwarven() {
+    public void generateNameUsingIntUnaryOperator() {
+        final IntUnaryOperator randomInRange = (value) -> new Random(System.nanoTime()).nextInt(value);
+
+        // Setup the Builder:
+        final CombinatorialNameGeneratorBuilder builder = new CombinatorialNameGeneratorBuilder();
+
         try {
-            final Random random = new Random(System.currentTimeMillis());
-
-            final CombinatorialNameGeneratorBuilder builderFirst = new CombinatorialNameGeneratorBuilder();
-            builderFirst.setBeginnings(TestHelper.resource("Dwarven/Realistic/Dwarven_Khordaldrum_Realistic_Male_A.txt"));
-            builderFirst.setEndings(TestHelper.resource("Dwarven/Realistic/Dwarven_Khordaldrum_Realistic_Male_B.txt"));
-
-            final CombinatorialNameGenerator firstNameGenerator = builderFirst.build(false);
-
-
-            final CombinatorialNameGeneratorBuilder builderLast = new CombinatorialNameGeneratorBuilder();
-            builderLast.setBeginnings(TestHelper.resource("Dwarven/Realistic/Dwarven_Khordaldrum_Realistic_Last_A.txt"));
-            builderLast.setEndings(TestHelper.resource("Dwarven/Realistic/Dwarven_Khordaldrum_Realistic_Last_B.txt"));
-
-            final CombinatorialNameGenerator lastNameGenerator = builderLast.build(false);
-
-            for(int i = 0 ; i < 50 ; i++) {
-                System.out.println(firstNameGenerator.generateName(random, i, 0) + " " + lastNameGenerator.generateName(random, i, 0));
-            }
-
-            for(int i = 0 ; i < 10000 ; i++) {
-                firstNameGenerator.generateName(random, i, 0);
-                lastNameGenerator.generateName(random, i, 0);
-            }
-        } catch(final IOException e) {
+            builder.setBeginnings(TestHelper.resource("Dwarven/Fantasy/Dwarven_Khordaldrum_Fantasy_Last_A.txt"));
+            builder.setEndings(TestHelper.resource("Dwarven/Fantasy/Dwarven_Khordaldrum_Fantasy_Last_B.txt"));
+        } catch (final IOException e) {
             e.printStackTrace();
+        }
+
+        // Setup & Test the Generator:
+        final CombinatorialNameGenerator generator = builder.build(false);
+
+        for (int i = 0 ; i < 100 ; i++) {
+            generator.generateName(randomInRange, i, 0);
+        }
+    }
+
+    @Test
+    public void longGeneration() {
+        final IntUnaryOperator randomInRange = (value) -> new Random(System.nanoTime()).nextInt(value);
+
+        // Setup the Builder:
+        final CombinatorialNameGeneratorBuilder builder = new CombinatorialNameGeneratorBuilder();
+
+        try {
+            builder.setBeginnings(TestHelper.resource("Dwarven/Fantasy/Dwarven_Khordaldrum_Fantasy_Last_A.txt"));
+            builder.setEndings(TestHelper.resource("Dwarven/Fantasy/Dwarven_Khordaldrum_Fantasy_Last_B.txt"));
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+
+        // Setup & Test the Generator:
+        final CombinatorialNameGenerator generator = builder.build(false);
+
+        for (int i = 0 ; i < 100 ; i++) {
+            System.out.println(generator.generateName(randomInRange, i, 0));
+        }
+
+        for (int i = 0 ; i < 1_000_000 ; i++) {
+            generator.generateName(randomInRange, i, 0);
         }
     }
 }
