8428d8fb1d98ad00ff1c714ffcabd0473b696ad8
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-06-22 15:06:59 -0300

    Removes use of Random and IntUnaryOperator..

--- test/com/valkryst/generator/MarkovNameGeneratorTest.java
+++ test/com/valkryst/generator/MarkovNameGeneratorTest.java
@@ -12,19 +12,17 @@ public class MarkovNameGeneratorTest {
     @Test
     public void generateName() {
         try {
-            final IntUnaryOperator randomInRange = ThreadLocalRandom.current()::nextInt;
-
             final MarkovBuilder builder = new MarkovBuilder();
             builder.setTrainingNames(TestHelper.resource("Human/Viking/Male.txt"));
 
             final MarkovNameGenerator nameGenerator = builder.build();
 
             for(int i = 0 ; i < 50 ; i++) {
-                System.out.println(nameGenerator.generateName(randomInRange, 7));
+                System.out.println(nameGenerator.generateName(7));
             }
 
             for(int i = 0 ; i < 10000 ; i++) {
-                nameGenerator.generateName(randomInRange, i % 50);
+                nameGenerator.generateName(i % 50);
             }
         } catch(final IOException e) {
             e.printStackTrace();
