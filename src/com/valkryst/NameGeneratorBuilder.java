2ce43e4ffcec0738c8cf550797798ed4798ffc0b
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-06-22 14:57:42 -0300

    Removes redundant code.

--- src/com/valkryst/NameGeneratorBuilder.java
+++ src/com/valkryst/NameGeneratorBuilder.java
@@ -1,16 +1,14 @@
 package com.valkryst;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 
 public class NameGeneratorBuilder {
     /**
-     * Reads each line of the specified file and returns them as a list. All empty lines are skipped.
+     * Reads each line of the specified file and returns them as a list.
+     * All empty lines are skipped.
      *
      * @param path
      *         The file to read from.
@@ -27,32 +25,9 @@ public class NameGeneratorBuilder {
             throw new IOException("The file at \"" + path.toString() +"\" does not exist.");
         }
 
-        // Attempt to read the lines using NIO.
-        // If this fails, then fallback to the BufferedReader method.
-        try {
-            return Files.readAllLines(path);
-        } catch (final IOException e) {
-            // Open Resources:
-            final FileReader fileReader = new FileReader(path.toFile());
-            final BufferedReader bufferedReader = new BufferedReader(fileReader);
+        final List<String> lines = Files.readAllLines(path);
+        lines.removeIf(String::isEmpty);
 
-            // Read Lines From File:
-            final List<String> readLines = new ArrayList<>();
-            String currentLine;
-
-            while((currentLine = bufferedReader.readLine()) != null) {
-                // Don't read empty lines:
-                if(! currentLine.isEmpty()) {
-                    readLines.add(currentLine);
-                }
-            }
-
-            // Close Resources:
-            bufferedReader.close();
-            fileReader.close();
-
-            // Return Read Lines:
-            return readLines;
-        }
+        return lines;
     }
 }
