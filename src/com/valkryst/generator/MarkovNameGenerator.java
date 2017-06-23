7567e0ed5ff87a6f44eb77e037a4fe4c97c1e963
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-06-23 11:33:19 -0300

    Rewrites most of the code for clarity and to work with the new MarkovChain class.

--- src/com/valkryst/generator/MarkovNameGenerator.java
+++ src/com/valkryst/generator/MarkovNameGenerator.java
@@ -2,18 +2,13 @@ package com.valkryst.generator;
 
 import com.valkryst.NameGenerator;
 import com.valkryst.builder.MarkovBuilder;
+import com.valkryst.markov.MarkovChain;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.Optional;
 
 public final class MarkovNameGenerator implements NameGenerator {
-    /** All two-character combinations found in the training names. */
-    private final List<String> sequences = new ArrayList<>();
-
-    // todo JavaDoc
-    private final MarkovChain<String, String> markovChain = new MarkovChain<>();
+    /** The chain to use when generating names. */
+    private final MarkovChain markovChain = new MarkovChain();
 
     /**
      * Constructs a MarkovNameGenerator.
@@ -22,109 +17,29 @@ public final class MarkovNameGenerator implements NameGenerator {
      *         Builder containing the training names.
      */
     public MarkovNameGenerator(final MarkovBuilder builder) {
-        builder.getTrainingNames().forEach(this::acquireProbabilities);
+        markovChain.train(builder.getTrainingNames());
     }
 
     @Override
-    public String generateName(final int length) {
+    public String generateName(int length) {
         if (length == 0) {
-            return "LENGTH_WAS_ZERO";
+            length = 2;
         }
 
         final StringBuilder sb = new StringBuilder();
-
-        // Choose a random preSequence to begin with:
-        String preSequence = sequences.get(ThreadLocalRandom.current().nextInt(sequences.size()));
-        sb.append(preSequence);
-
-        String previous = preSequence.substring(0, 1);
-        String current = preSequence.substring(1, 2);
+        sb.append(markovChain.chooseRandomString()); // Begin the name with a random pair of two characters.
 
         for (int i = 2; i < length; ++i) {
-            preSequence  = previous + "" + current;
+            final Optional<Character> next = markovChain.chooseRandomCharacter(sb.substring(i - 2, i));
 
-            try {
-                final String next = chooseNextCharacter(preSequence);
-                sb.append(next);
-                previous = current;
-                current = next;
-            } catch (final NoSuchElementException e) {
+            if (next.isPresent()) {
+                sb.append(next.get());
+            } else {
                 break;
             }
-
         }
 
         // Capitalize the first letter of the name:
         return sb.toString().substring(0, 1).toUpperCase() + sb.toString().substring(1);
-    }
-
-    /**
-     * Parses the specified string to determine the probability of a character appearing after the previous two
-     * characters beginning with the third character in the string and ending with the last.
-     *
-     * @param trainingString
-     *         A string to parse.
-     */
-    public void acquireProbabilities(final String trainingString) {
-        if (trainingString.length() < 2) {
-            return;
-        }
-
-        for (int i = 2 ; i < trainingString.length(); ++i) {
-            final String preSequence = trainingString.substring(i - 2, i);
-            final String sequence = trainingString.substring(i, i + 1);
-
-            if (sequences.contains(preSequence) == false) {
-                 sequences.add(preSequence);
-            }
-
-            markovChain.incrementOccurrences(preSequence, sequence);
-        }
-    }
-
-    /**
-     * Determines the sequence to follow the specified pre-sequence using the precomputed probabilities of which
-     * sequences most often appear after the specified pre-sequence.
-     *
-     * @param preSequence
-     *         The pre-sequence to find the next character for.
-     *
-     * @return
-     *         The next character of the sequence.
-     *
-     * @throws NoSuchElementException
-     *          If the specified pre-sequence has no corresponding probabilities to determine the next sequence from.
-     */
-    private String chooseNextCharacter(final String preSequence) throws NoSuchElementException {
-        final ArrayList<String> sequences = markovChain.getAllSequences(preSequence);
-
-        if (sequences.size() == 0) {
-            throw new NoSuchElementException("There are no computed probabilities for the specified pre-sequence.");
-        }
-
-        Float highestProbability = null;
-        final List<String> highestStrings = new ArrayList<>();
-
-        for(final String sequence : sequences) {
-            final Float currentProbability = markovChain.getProbability(preSequence, sequence);
-
-            // If the initial data has not yet been set,
-            // then set it.
-            if (highestProbability == null) {
-                highestStrings.add(sequence);
-                highestProbability = currentProbability;
-            } else {
-                if(currentProbability > highestProbability) {
-                    highestStrings.clear();
-                    highestStrings.add(sequence);
-                    highestProbability = currentProbability;
-                } else {
-                    highestStrings.add(sequence);
-                }
-            }
-        }
-
-        final int randomIndex = ThreadLocalRandom.current().nextInt(highestStrings.size());
-        return highestStrings.get(randomIndex);
     }
 }
