fdb3dd61e02423827617244408d9a54ca75e88ce
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-06-23 15:59:56 -0300

    Removes unnecessary code.

--- src/com/valkryst/generator/GrammarNameGenerator.java
+++ src/com/valkryst/generator/GrammarNameGenerator.java
@@ -1,107 +1,49 @@
 package com.valkryst.generator;
 
+import com.valkryst.VParser_CFG.ContextFreeGrammar;
+
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public final class GrammarNameGenerator implements NameGenerator{
-    /** The name generation rules. */
-    private String[] rules;
+    private final ContextFreeGrammar contextFreeGrammar;
 
     /**
      * Constructs a GrammarNameGenerator.
      *
      * @param rules
      *         The name generation rules.
+     *
+     * @throws IllegalArgumentException
+     *         If there is a semantic error in one of the rules.
      */
     public GrammarNameGenerator(final List<String> rules) {
-        this.rules = rules.toArray(new String[rules.size()]);
+        contextFreeGrammar = new ContextFreeGrammar(rules);
     }
 
 
     @Override
     public String generateName(final int length) {
-        final char startingSymbol = (char) ThreadLocalRandom.current().nextInt(Character.MAX_VALUE);
-        return generateName(length, startingSymbol);
-    }
+        String longestResult = "";
 
-    /**
-     * Attempts to generate a name of at-least the specified length.
-     *
-     * @param length
-     *         The length of the name to generateName.
-     *
-     *         If the value is less than or equal to zero, then this parameter is ignored.
-     *
-     *         No guarantee is made that the name will be exactly this length.
-     *
-     * @param startingSymbol
-     *         The symbol to begin generating the name from.
-     *
-     * @return
-     *         The generated name.
-     */
-    public String generateName(final int length, final char startingSymbol) {
-        if (length == 0) {
-            return "LENGTH_WAS_ZERO";
-        }
+        for (int i = 0 ; i < 100 ; i++) {
+            final String tmp = contextFreeGrammar.run();
 
-        final StringBuilder sb = new StringBuilder();
-        sb.append(startingSymbol);
+            if (tmp.length() > longestResult.length()) {
+                longestResult = tmp;
+            }
 
-        for (int i = 0; i < sb.length(); i++) {
-            final char currentChar = sb.charAt(i);
-            final boolean isCurrentCharUpperCase = Character.isUpperCase(currentChar);
+            if (tmp.length() > length) {
+                longestResult = tmp.substring(0, length);
+                break;
+            }
 
-            if (isCurrentCharUpperCase) {
-                final String substitution = chooseRandomRuleOption(currentChar);
-
-                if (substitution != null) {
-                    String temp = sb.toString();
-                    temp = temp.replace(String.valueOf(currentChar), substitution);
-
-                    sb.setLength(0);
-                    sb.append(temp);
-
-                    i--;
-                }
+            if (tmp.length() == length) {
+                longestResult = tmp;
+                break;
             }
         }
 
-        // Return Name:
-        return sb.toString();
-    }
-
-    /**
-     * Searches the rules of the grammar for the first rule whose first character matches that of the specified
-     * character.
-     *
-     * Ex:
-     *      character = 'S'
-     *
-     *      rule = "T tt Tt"
-     *      rule = "S ss Ss"
-     *      rule = "S tt St"
-     *
-     *      The first rule is ignored and the second rule is used.
-     *
-     * @param character
-     *         The identifier of the grammar rule to use.
-     *
-     * @return
-     *         The chosen rule or null if no rule could be chosen.
-     */
-    private String chooseRandomRuleOption(final char character) {
-        for (final String rule : rules) {
-            final String[] tokens = rule.split(" ");
-
-            if (tokens[0].equals(String.valueOf(character))) {
-                if (tokens.length > 1) {
-                    final int randomOptionIndex = ThreadLocalRandom.current().nextInt(tokens.length - 1) + 1;
-                    return tokens[randomOptionIndex];
-                }
-            }
-        }
-
-        return null;
+        return longestResult;
     }
 }
