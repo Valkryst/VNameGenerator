a4c58bd991a638dda517f9506480b65f66b17ad6
Valkryst <valkryst@valkryst.com>
2017-06-24 13:22:00 -0300

    Adds actual unit tests.

--- test/com/valkryst/generator/CombinatorialNameGeneratorTest.java
+++ test/com/valkryst/generator/CombinatorialNameGeneratorTest.java
@@ -1,56 +1,100 @@
 package com.valkryst.generator;
 
-import com.valkryst.TestHelper;
-import com.valkryst.builder.CombinatorialBuilder;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.function.IntUnaryOperator;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CombinatorialNameGeneratorTest {
-    @Test
-    public void generateName() {
-        // Setup the Builder:
-        final CombinatorialBuilder builder = new CombinatorialBuilder();
+    private final List<String> beginnings;
+    private final List<String> middles;
+    private final List<String> endings;
 
-        try {
-            builder.setBeginnings(TestHelper.resource("Dwarven/Fantasy/Khordaldrum_Last_A.txt"));
-            builder.setEndings(TestHelper.resource("Dwarven/Fantasy/Khordaldrum_Last_B.txt"));
-        } catch (final IOException e) {
-            e.printStackTrace();
-        }
+    public CombinatorialNameGeneratorTest() throws URISyntaxException, IOException {
+        final ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        URL resourceUrl = classloader.getResource("Titles/Dune/Titles.txt");
+        beginnings = Files.readAllLines(Paths.get(resourceUrl.toURI()));
 
-        // Setup & Test the Generator:
-        final CombinatorialNameGenerator generator = builder.build();
+        resourceUrl = classloader.getResource("Dwarven/Fantasy/Khordaldrum_Last_A.txt");
+        middles = Files.readAllLines(Paths.get(resourceUrl.toURI()));
 
-        for (int i = 0 ; i < 100 ; i++) {
-            generator.generateName(i % 20);
-        }
+        resourceUrl = classloader.getResource("Dwarven/Fantasy/Khordaldrum_Last_B.txt");
+        endings = Files.readAllLines(Paths.get(resourceUrl.toURI()));
     }
 
     @Test
-    public void longGeneration() {
-        // Setup the Builder:
-        final CombinatorialBuilder builder = new CombinatorialBuilder();
+    public void testConstructor_twoParams_withValidInput() {
+        new CombinatorialNameGenerator(beginnings, endings);
+    }
 
-        try {
-            builder.setBeginnings(TestHelper.resource("Dwarven/Fantasy/Khordaldrum_Last_A.txt"));
-            builder.setEndings(TestHelper.resource("Dwarven/Fantasy/Khordaldrum_Last_B.txt"));
-        } catch (final IOException e) {
-            e.printStackTrace();
-        }
+    @Test
+    public void testConstructor_threeParams_withValidInput() {
+        new CombinatorialNameGenerator(beginnings, middles, endings);
+    }
 
-        // Setup & Test the Generator:
-        final CombinatorialNameGenerator generator = builder.build();
+    @Test(expected=IllegalArgumentException.class)
+    public void testConstructor_threeParams_withNullBeginnings() {
+        new CombinatorialNameGenerator(null, middles, endings);
+    }
 
-        for (int i = 0 ; i < 100 ; i++) {
-            System.out.println(generator.generateName(i));
-        }
+    @Test
+    public void testConstructor_threeParams_withNullMiddles() {
+        new CombinatorialNameGenerator(beginnings, null, endings);
+    }
 
-        for (int i = 0 ; i < 1_000_000 ; i++) {
-            generator.generateName(i % 20);
+    @Test(expected=IllegalArgumentException.class)
+    public void testConstructor_threeParams_withNullEndings() {
+        new CombinatorialNameGenerator(beginnings, middles, null);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testConstructor_threeParams_withEmptyBeginnings() {
+        new CombinatorialNameGenerator(new ArrayList<>(0), middles, endings);
+    }
+
+    @Test
+    public void testConstructor_threeParams_withEmptyMiddles() {
+        new CombinatorialNameGenerator(beginnings, new ArrayList<>(0), endings);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testConstructor_threeParams_withEmptyEndings() {
+        new CombinatorialNameGenerator(beginnings, middles, new ArrayList<>(0));
+    }
+
+    @Test
+    public void testGenerateName_withZeroLength() {
+        final CombinatorialNameGenerator nameGenerator = new CombinatorialNameGenerator(beginnings, middles, endings);
+        final String result = nameGenerator.generateName(0);
+        Assert.assertTrue(result.length() >= 2);
+    }
+
+    @Test
+    public void  testGenerateName_withOneLength() {
+        final CombinatorialNameGenerator nameGenerator = new CombinatorialNameGenerator(beginnings, middles, endings);
+        final String result = nameGenerator.generateName(1);
+        Assert.assertTrue(result.length() >= 2);
+    }
+
+    @Test
+    public void  testGenerateName_withTwoLength() {
+        final CombinatorialNameGenerator nameGenerator = new CombinatorialNameGenerator(beginnings, middles, endings);
+        final String result = nameGenerator.generateName(2);
+        Assert.assertTrue(result.length() >= 2);
+    }
+
+    @Test
+    public void  testGenerateName_withThreeToTwentyLength() {
+        final CombinatorialNameGenerator nameGenerator = new CombinatorialNameGenerator(beginnings, middles, endings);
+        for (int i = 3 ; i < 20 ; i++) {
+            final String result = nameGenerator.generateName(i);
+            Assert.assertTrue(result.length() >= i);
         }
     }
 }
