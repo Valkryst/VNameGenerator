8795aee1be059ed699a7decda56e249dc229938d
Valkryst <valkryst@valkryst.com>
2017-06-24 12:46:34 -0300

    Adds actual tests.

--- test/com/valkryst/generator/MarkovNameGeneratorTest.java
+++ test/com/valkryst/generator/MarkovNameGeneratorTest.java
@@ -1,31 +1,70 @@
 package com.valkryst.generator;
 
-import com.valkryst.TestHelper;
-import com.valkryst.builder.MarkovBuilder;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.function.IntUnaryOperator;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MarkovNameGeneratorTest {
+    private MarkovNameGenerator nameGenerator;
+    private final List<String> trainingNames;
+
+    public MarkovNameGeneratorTest() throws URISyntaxException, IOException {
+        final ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        final URL resourceUrl = classloader.getResource("Human/Viking/Male.txt");
+        trainingNames = Files.readAllLines(Paths.get(resourceUrl.toURI()));
+    }
+
+    @Before
+    public void initializeVariables() {
+        nameGenerator = new MarkovNameGenerator(trainingNames);
+    }
+
     @Test
-    public void generateName() {
-        try {
-            final MarkovBuilder builder = new MarkovBuilder();
-            builder.setTrainingNames(TestHelper.resource("Human/Viking/Male.txt"));
+    public void testConstructor_withValidTrainingNames() {
+        nameGenerator = new MarkovNameGenerator(trainingNames);
+    }
 
-            final MarkovNameGenerator nameGenerator = builder.build();
+    @Test(expected=IllegalArgumentException.class)
+    public void testConstructor_withNullTrainingNames() {
+        nameGenerator = new MarkovNameGenerator(null);
+    }
 
-            for(int i = 0 ; i < 50 ; i++) {
-                System.out.println(nameGenerator.generateName(7));
-            }
+    @Test(expected=IllegalArgumentException.class)
+    public void testConstructor_withEmptyTrainingNames() {
+        nameGenerator = new MarkovNameGenerator(new ArrayList<>(0));
+    }
 
-            for(int i = 0 ; i < 10000 ; i++) {
-                nameGenerator.generateName(i % 50);
-            }
-        } catch(final IOException e) {
-            e.printStackTrace();
+    @Test
+    public void testGenerateName_withZeroLength() {
+        final String result = nameGenerator.generateName(0);
+        Assert.assertEquals(2, result.length());
+    }
+
+    @Test
+    public void  testGenerateName_withOneLength() {
+        final String result = nameGenerator.generateName(1);
+        Assert.assertEquals(2, result.length());
+    }
+
+    @Test
+    public void  testGenerateName_withTwoLength() {
+        final String result = nameGenerator.generateName(2);
+        Assert.assertEquals(2, result.length());
+    }
+
+    @Test
+    public void  testGenerateName_withThreeToTwentyLength() {
+        for (int i = 3 ; i < 20 ; i++) {
+            final String result = nameGenerator.generateName(i);
+            Assert.assertEquals(i, result.length());
         }
     }
 }
