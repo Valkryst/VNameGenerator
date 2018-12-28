e2909eeeb1f872b620a715198c7ce38b70717594
Valkryst <valkryst@valkryst.com>
2018-12-28 11:42:28 -0400

    Updates ConsonantVowelGenerator tests to work with the new NameGenerator methods and changes to the ConsonantVowelGenerator class.

--- test/com/valkryst/generator/ConsonantVowelGeneratorTest.java
+++ test/com/valkryst/generator/ConsonantVowelGeneratorTest.java
@@ -8,109 +8,63 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ConsonantVowelGeneratorTest {
-    private final List<String> consonants = new ArrayList<>();
-    private final List<String> vowels = new ArrayList<>();
+    private final String[] consonants = new String[] {
+        "al", "an", "ar", "as", "at", "ea", "ed", "en", "er", "es", "ha", "he", "hi", "in", "is", "it",
+        "le", "me", "nd", "ne", "ng", "nt", "on", "or", "ou", "re", "se", "st", "te", "th", "ti", "to",
+        "ve", "wa", "it"
+    };
 
-    private ConsonantVowelGenerator nameGenerator;
-
-    @Before
-    public void initializeVariables() {
-        consonants.clear();
-        vowels.clear();
-
-        consonants.add("al");
-        consonants.add("an");
-        consonants.add("ar");
-        consonants.add("as");
-        consonants.add("at");
-        consonants.add("ea");
-        consonants.add("ed");
-        consonants.add("en");
-        consonants.add("er");
-        consonants.add("es");
-        consonants.add("ha");
-        consonants.add("he");
-        consonants.add("hi");
-        consonants.add("in");
-        consonants.add("is");
-        consonants.add("it");
-        consonants.add("le");
-        consonants.add("me");
-        consonants.add("nd");
-        consonants.add("ne");
-        consonants.add("ng");
-        consonants.add("nt");
-        consonants.add("on");
-        consonants.add("or");
-        consonants.add("ou");
-        consonants.add("re");
-        consonants.add("se");
-        consonants.add("st");
-        consonants.add("te");
-        consonants.add("th");
-        consonants.add("ti");
-        consonants.add("to");
-        consonants.add("ve");
-        consonants.add("wa");
-        consonants.add("it");
-
-        vowels.add("a");
-        vowels.add("e");
-        vowels.add("i");
-        vowels.add("o");
-        vowels.add("u");
-        vowels.add("y");
-
-        nameGenerator = new ConsonantVowelGenerator(consonants, vowels);
-    }
+    private final String[] vowels = new String[] {
+            "a", "e", "i", "o", "u", "y"
+    };
 
     @Test
     public void testConstructor_withValidConsonantsAndVowels() {
-        nameGenerator = new ConsonantVowelGenerator(consonants, vowels);
+        new ConsonantVowelGenerator(consonants, vowels);
     }
 
     @Test(expected=IllegalArgumentException.class)
     public void testConstructor_withNullConsonants() {
-        nameGenerator = new ConsonantVowelGenerator(null, vowels);
+        new ConsonantVowelGenerator(null, vowels);
     }
 
     @Test(expected=IllegalArgumentException.class)
     public void testConstructor_withNullVowels() {
-        nameGenerator = new ConsonantVowelGenerator(consonants, null);
+        new ConsonantVowelGenerator(consonants, null);
     }
 
     @Test(expected=IllegalArgumentException.class)
     public void testConstructor_withEmptyConsonants() {
-        nameGenerator = new ConsonantVowelGenerator(new ArrayList<>(0), vowels);
+        new ConsonantVowelGenerator(new String[0], vowels);
     }
 
     @Test(expected=IllegalArgumentException.class)
     public void testConstructor_withEmptyVowels() {
-        nameGenerator = new ConsonantVowelGenerator(consonants, new ArrayList<>(0));
+        new ConsonantVowelGenerator(consonants, new String[0]);
     }
 
     @Test
     public void testGenerateName_withZeroLength() {
-        final String result = nameGenerator.generateName(0);
+        final String result = new ConsonantVowelGenerator(consonants, vowels).generateName(0);
         Assert.assertEquals(2, result.length());
     }
 
     @Test
     public void  testGenerateName_withOneLength() {
-        final String result = nameGenerator.generateName(1);
+        final String result = new ConsonantVowelGenerator(consonants, vowels).generateName(1);
         Assert.assertEquals(2, result.length());
     }
 
     @Test
     public void  testGenerateName_withTwoLength() {
-        final String result = nameGenerator.generateName(2);
+        final String result = new ConsonantVowelGenerator(consonants, vowels).generateName(2);
         Assert.assertEquals(2, result.length());
     }
 
     @Test
     public void  testGenerateName_withThreeToTwentyLength() {
         for (int i = 3 ; i < 20 ; i++) {
-            final String result = nameGenerator.generateName(i);
+            final String result = new ConsonantVowelGenerator(consonants, vowels).generateName(i);
             Assert.assertEquals(i, result.length());
         }
     }
