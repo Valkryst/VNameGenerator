29ea9e4db27f50196bc9c770706a5984bd6e583e
Valkryst <valkryst@valkryst.com>
2021-12-19 11:59:53 -0400

    Move `MarkovChain` and `Entry` into `MarkovGenerator`

--- src/main/java/com/valkryst/VNameGenerator/generator/MarkovGenerator.java
+++ src/main/java/com/valkryst/VNameGenerator/generator/MarkovGenerator.java
@@ -1,8 +1,10 @@
 package com.valkryst.VNameGenerator.generator;
 
-import com.valkryst.VNameGenerator.markov.MarkovChain;
 import lombok.NonNull;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 
 public final class MarkovGenerator extends NameGenerator {
@@ -58,5 +60,103 @@ public final class MarkovGenerator extends NameGenerator {
 
     	markovChain = new MarkovChain();
     	markovChain.train(names);
+	}
+
+	private final class Entry {
+		/** The characters.*/
+		private char[] characters;
+		/** The threshold value of each character. */
+		private int[] threshold;
+
+		public Entry(final @NonNull Map<Character, Integer> occurrences) {
+			characters = new char[occurrences.size()];
+			threshold = new int[occurrences.size()];
+
+			int counter = 0;
+			for (final Map.Entry<Character, Integer> entry : occurrences.entrySet()) {
+				characters[counter] = entry.getKey();
+				threshold[counter] = entry.getValue() + (counter > 0 ? threshold[counter - 1] : 0);
+
+				counter++;
+			}
+		}
+
+		public Optional<Character> chooseCharacter() {
+			final int rand = ThreadLocalRandom.current().nextInt(threshold[threshold.length - 1] + 1);
+
+			for (int i = 0 ; i < threshold.length ; i++) {
+				if (threshold[i] >= rand) {
+					return Optional.of(characters[i]);
+				}
+			}
+
+			return Optional.empty();
+		}
+	}
+
+	private final class MarkovChain {
+		private final Map<String, Entry> entries = new HashMap<>();
+
+		/**
+		 * Trains the Markov Chain using a set of strings.
+		 *
+		 * @param strings
+		 *          The training strings.
+		 */
+		public void train(final String[] strings) {
+			final Map<String, Map<Character, Integer>> builders = new HashMap<>();
+
+			for (final String string : strings) {
+				if (string.length() < 2) {
+					return;
+				}
+
+				for (int i = 2 ; i < string.length() ; i++) {
+					final String charGroup = string.substring(i - 2, i);
+					final char subsequentChar = string.charAt(i);
+
+					builders.putIfAbsent(charGroup, new HashMap<>());
+
+					builders.get(charGroup).putIfAbsent(subsequentChar, 0);
+					builders.get(charGroup).put(string.charAt(i), builders.get(charGroup).get(subsequentChar) + 1);
+				}
+			}
+
+			for (final Map.Entry<String, Map<Character, Integer>> entry : builders.entrySet()) {
+				entries.put(entry.getKey(), new Entry(entry.getValue()));
+			}
+		}
+
+		/**
+		 * Chooses a random character to follow a string.
+		 *
+		 * @param string
+		 *          The string.
+		 *
+		 * @return
+		 *          The character.
+		 */
+		public Optional<Character> chooseRandomCharacter(final String string) {
+			if (string == null || string.isEmpty()) {
+				return Optional.empty();
+			}
+
+			if (entries.containsKey(string)) {
+				return entries.get(string).chooseCharacter();
+			} else {
+				return Optional.empty();
+			}
+		}
+
+		/**
+		 * Chooses a random string from the set of valid strings.
+		 *
+		 * @return
+		 *          The string.
+		 */
+		public String chooseRandomString() {
+			final int index = ThreadLocalRandom.current().nextInt(entries.size());
+			return (String) entries.keySet().toArray()[index];
+		}
 	}
 }
